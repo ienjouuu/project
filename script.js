88b2cda1d6ece64554441ddc551dfc6cd77d517d
Sergio <ienjou@gmail.com>
2020-03-09 20:31:59 +0300

    fouth  commit

--- script.js
+++ script.js
@@ -4,10 +4,8 @@
 // confirm('Are u married?');
 //prompt(gi
 
-var money = prompt('Ваш бюджет на месяц?');
+var money = +prompt('Ваш бюджет на месяц?');
 var time = prompt('Введите дату в формате YYYY-MM-DD');
-
-
 
 let appData = {
     "budget": money,
@@ -18,9 +16,30 @@ let appData = {
     "saving": false
 };
 
-let expenses1 = prompt('Введите обязательную статью расходов в этом месяце');
-let expenses2 = prompt('Во сколько обойдется');
 
-appData.expenses[expenses1] = expenses2;
-alert(money / 30);
-console.log(appData);
+for (let i = 0; i < 2; i++) {
+    let a = prompt('Введите обязательную статью расходов в этом месяце'),
+        b = prompt('Во сколько обойдется');
+    if (typeof (a) === 'string' && typeof (a) != null && typeof (b) != null &&
+        a != '' && b != '' && a.length < 50) {
+        console.log('done');150
+        appData.expenses[a] = b;
+    } else {
+        console.log('bad');
+    }
+
+}
+
+appData.moneyPerDay = appData.budget / 30;
+
+alert('Ежедневный бюджет: ' + appData.moneyPerDay);
+
+if (appData.moneyPerDay < 100) {
+    console.log('Минимальный уровень');
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log('Средний уровень');
+} else if (appData.moneyPerDay > 2000) {
+    console.log('Высокий уровень');
+} else {
+    console.log('ERROR');
+}
